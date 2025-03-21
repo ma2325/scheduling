247edc3f9224f7884ff054be4f7d2f809e90f322
Camelli-a <152971131+Camelli-a@users.noreply.github.com>
2025-03-21 16:44:53 +0800

    数据读取修改

--- ai/main.py
+++ ai/main.py
@@ -8,36 +8,38 @@ cursor=conn.cursor()
 
 #载入课程
 def load_course():
-    cursor.execute("SELECT coid,cotype,covolume FROM course")
+    cursor.execute("SELECT coid,cotype2,codepartment FROM course")
     courses=[]#列表储存
     for row in cursor.fetchall():
         course=sql.models.Course(
             coid=row['coid'],
-            cotype=row['cotype'],
-            covolume=row['covolume']
+            cotype2=row['cotype2'],
+            codepartment=row['codepartment']
         )
         courses.append(course)
     return courses
 
 #载入教室
 def load_room():
-    cursor.execute("SELECT rid,rvolume FROM room")
+    cursor.execute("SELECT rid,rtype,rcapacity FROM room")
     rooms=[]
     for row in cursor.fetchall():
         room=sql.models.Room(
             rid=row['rid'],
-            rvolume=row['rvolume']
+            rtype=row['rtype'],
+            rcapacity=row['rcapacity']
         )
         rooms.append(room)
     return rooms
 
 #载入老师
 def load_teacher():
-    cursor.execute("SELECT tid FROM teacher")
+    cursor.execute("SELECT tcode,tdepartment FROM teacher")
     teachers=[]
     for row in cursor.fetchall():
         teacher=sql.models.Teacher(
-            tid=row['tid']
+            tcode=row['tcode'],
+            tdepartment=row['tdepartment']
         )
         teachers.append(teacher)
     return teachers
@@ -47,20 +49,26 @@ if __name__ == "__main__":
     try:
         courses=load_course()
         print("courses:")
+        '''
         for course in courses:
-            print(f"ID: {course.coid}, Type: {course.cotype}, Volume: {course.covolume}")
+            print(f"ID: {course.coid}, Type: {course.cotype2}")
+        '''
         print("courses OK\n")
 
         rooms=load_room()
         print("rooms: ")
+        '''
         for room in rooms:
-            print(f"ID: {room.rid}, Volume:{room.rvolume}")
+            print(f"ID: {room.rid}, Volume:{room.rvolume}, Type: {room.rtype}")
+        '''
         print("rooms OK\n")
 
         teachers=load_teacher()
+        '''
         print("teachers: ")
         for teacher in teachers:
-            print(f"ID:{teacher.tid}")
+            print(f"ID:{teacher.tcode}")
+        '''
         print("teachers OK\n")
     finally:
         #关闭
@@ -69,6 +77,8 @@ if __name__ == "__main__":
         conn.close()
 
     print("OK")
+    '''
     shedule= inheritance.Timetable.result(courses, rooms, teachers)
     for eachclass in shedule:
         print(eachclass)
+    '''
--- ai/sql/models.py
+++ ai/sql/models.py
@@ -1,44 +1,32 @@
 #地点：
 #教室
 class Room:
-    def __init__(self,rid,rvolume):
+    def __init__(self,rid,rtype,rcapacity):
         self.rid = rid
-        self.rvolume = rvolume
-
-
-#学院
-class College:
-    def __init__(self, cid):
-        self.cid = cid
-
-#系
-class Department:
-    def __init__(self, did, dcollege):
-        self.did = did
-        self.dcollege = dcollege
+        self.rtype = rtype
+        self.rcapacity = rcapacity
 
 #人物：
 
 #老师
 class Teacher:
-    def __init__(self, tid):
-        self.tid = tid
+    def __init__(self, tcode,tdepartment):
+        self.tcode = tcode
+        self.tdepartment = tdepartment
 
 #课：
 #课程
 class Course:
-    def __init__(self, coid, cotype, covolume):
+    def __init__(self, coid, cotype2,codepartment):
         self.coid = coid
-        self.cotype = cotype
-        self.covolume = covolume
-
+        self.cotype2 = cotype2
+        self.codepartment = codepartment
 
 #结果：
 #课程表
 class Schedule:
-    def __init__(self, scid, sctask, scroom, tabegin_week, taend_week, tabegin_time, taend_time):
+    def __init__(self, scid,  scroom, tabegin_week, taend_week, tabegin_time, taend_time):
         self.scid = scid
-        self.sctask = sctask
         self.scroom = scroom
         self.tabegin_week = tabegin_week
         self.taend_week = taend_week
